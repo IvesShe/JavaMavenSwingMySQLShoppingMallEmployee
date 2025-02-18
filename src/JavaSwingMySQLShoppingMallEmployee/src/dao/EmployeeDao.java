c85f6a63187cefee3a17eeb1ae5011b348e04172
ives <ivesshe@gmail.com>
2025-02-18 15:18:43 +0800

    update

---
+++ src/JavaSwingMySQLShoppingMallEmployee/src/dao/EmployeeDao.java
@@ -0,0 +1,23 @@
+package dao;
+
+import java.util.List;
+
+import model.Employee;
+
+public interface EmployeeDao {
+	
+	// 新增員工
+	void add(Employee employee);
+	
+	// 查詢員工
+	List<Employee> selectAll();
+	List<Employee> selectUsernameAndPassword(String Username,String password);
+	List<Employee> selectById(int id);
+	List<Employee> selectByUsername(String username);
+	
+	// 更新員工
+	void update(Employee employee);
+	
+	// 刪除員工
+	void delete(int id);
+}
---
+++ src/JavaSwingMySQLShoppingMallEmployee/src/dao/impl/EmployeeDaoImpl.java
@@ -0,0 +1,251 @@
+package dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import dao.EmployeeDao;
+import model.Employee;
+import util.Tool;
+
+public class EmployeeDaoImpl implements EmployeeDao{
+
+	private static Connection conn = util.DbConnection.getDB();
+	
+	public static void main(String[] args) {
+		
+//		Employee employee = new Employee("e001","tom123","123","tom","389","台中");
+//		new EmployeeDaoImpl().add(employee);
+		
+//		List<Employee> employeeList = new EmployeeDaoImpl().selectAll();
+//		for(Employee e:employeeList)
+//		{
+//			String show =String.format(
+//				    "id: %-5d EmployeeNo: %-6s Name: %-10s Username: %-10s Password: %-10s Phone: %-10s Address: %-10s 建立時間: %s 更新時間: %s",
+//				    e.getId(),
+//				    e.getEmployeeNo(),
+//				    e.getName(),
+//				    e.getUsername(),
+//				    e.getPassword(),
+//				    e.getPhone(),
+//				    e.getAddress(),
+//				    Tool.formatTimestamp(e.getCreatedAt()),
+//				    Tool.formatTimestamp(e.getUpdatedAt())
+//				);
+//			System.out.println(show);
+//		}
+		
+//		List<Employee> employeeList = new EmployeeDaoImpl().selectUsernameAndPassword("andy123","1234");
+//		for(Employee e:employeeList)
+//		{
+//			String show =String.format(
+//				    "id: %-5d EmployeeNo: %-6s Name: %-10s Username: %-10s Password: %-10s Phone: %-10s Address: %-10s 建立時間: %s 更新時間: %s",
+//				    e.getId(),
+//				    e.getEmployeeNo(),
+//				    e.getName(),
+//				    e.getUsername(),
+//				    e.getPassword(),
+//				    e.getPhone(),
+//				    e.getAddress(),
+//				    Tool.formatTimestamp(e.getCreatedAt()),
+//				    Tool.formatTimestamp(e.getUpdatedAt())
+//				);
+//			System.out.println(show);
+//		}
+		
+//		List<Employee> employeeList = new EmployeeDaoImpl().selectById(1);
+//		for(Employee e:employeeList)
+//		{
+//			String show =String.format(
+//				    "id: %-5d EmployeeNo: %-6s Name: %-10s Username: %-10s Password: %-10s Phone: %-10s Address: %-10s 建立時間: %s 更新時間: %s",
+//				    e.getId(),
+//				    e.getEmployeeNo(),
+//				    e.getName(),
+//				    e.getUsername(),
+//				    e.getPassword(),
+//				    e.getPhone(),
+//				    e.getAddress(),
+//				    Tool.formatTimestamp(e.getCreatedAt()),
+//				    Tool.formatTimestamp(e.getUpdatedAt())
+//				);
+//			System.out.println(show);
+//		}
+		
+		List<Employee> employeeList = new EmployeeDaoImpl().selectByUsername("tom123");
+		for(Employee e:employeeList)
+		{
+			String show =String.format(
+				    "id: %-5d EmployeeNo: %-6s Name: %-10s Username: %-10s Password: %-10s Phone: %-10s Address: %-10s 建立時間: %s 更新時間: %s",
+				    e.getId(),
+				    e.getEmployeeNo(),
+				    e.getName(),
+				    e.getUsername(),
+				    e.getPassword(),
+				    e.getPhone(),
+				    e.getAddress(),
+				    Tool.formatTimestamp(e.getCreatedAt()),
+				    Tool.formatTimestamp(e.getUpdatedAt())
+				);
+			System.out.println(show);
+		}
+
+	}
+
+	@Override
+	public void add(Employee employee) {
+		String sql = "insert into employee (employee_no,username,password,name,phone,address) values(?,?,?,?,?,?)";
+		try {
+			PreparedStatement preparedStatement = conn.prepareStatement(sql);
+			preparedStatement.setString(1, employee.getEmployeeNo());
+			preparedStatement.setString(2, employee.getUsername());
+			preparedStatement.setString(3, employee.getPassword());
+			preparedStatement.setString(4, employee.getName());
+			preparedStatement.setString(5, employee.getPhone());
+			preparedStatement.setString(6, employee.getAddress());
+			preparedStatement.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public List<Employee> selectAll() {
+		String sql = "select * from employee";
+		List<Employee> employeeList = new ArrayList<>();
+		
+		try {
+			PreparedStatement preparedStatement = conn.prepareStatement(sql);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			while(resultSet.next())
+			{
+				Employee employee = new Employee();
+				employee.setEmployeeNo(resultSet.getString("employee_no"));
+				employee.setUsername(resultSet.getString("username"));
+				employee.setPassword(resultSet.getString("password"));
+				employee.setName(resultSet.getString("name"));
+				employee.setPhone(resultSet.getString("phone"));
+				employee.setAddress(resultSet.getString("address"));
+				employee.setCreatedAt(resultSet.getTimestamp("created_at"));
+				employee.setUpdatedAt(resultSet.getTimestamp("updated_at"));
+				employee.setId(resultSet.getInt("id"));
+				employeeList.add(employee);
+			}				
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+
+		return employeeList;
+	}
+
+	@Override
+	public List<Employee> selectUsernameAndPassword(String Username, String password) {
+		String sql = "select * from employee where username=? and password=?";
+		List<Employee> employeeList = new ArrayList<>();
+		
+		try {
+			PreparedStatement preparedStatement = conn.prepareStatement(sql);
+			preparedStatement.setString(1, Username);
+			preparedStatement.setString(2, password);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			while(resultSet.next())
+			{
+				Employee employee = new Employee();
+				employee.setEmployeeNo(resultSet.getString("employee_no"));
+				employee.setUsername(resultSet.getString("username"));
+				employee.setPassword(resultSet.getString("password"));
+				employee.setName(resultSet.getString("name"));
+				employee.setPhone(resultSet.getString("phone"));
+				employee.setAddress(resultSet.getString("address"));
+				employee.setCreatedAt(resultSet.getTimestamp("created_at"));
+				employee.setUpdatedAt(resultSet.getTimestamp("updated_at"));
+				employee.setId(resultSet.getInt("id"));
+				employeeList.add(employee);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return employeeList;
+	}
+
+	@Override
+	public List<Employee> selectById(int id) {
+		String sql = "select * from employee where id=?";
+		List<Employee> employeeList = new ArrayList<>();
+		
+		try {
+			PreparedStatement preparedStatement = conn.prepareStatement(sql);
+			preparedStatement.setInt(1, id);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			while(resultSet.next())
+			{
+				Employee employee = new Employee();
+				employee.setEmployeeNo(resultSet.getString("employee_no"));
+				employee.setUsername(resultSet.getString("username"));
+				employee.setPassword(resultSet.getString("password"));
+				employee.setName(resultSet.getString("name"));
+				employee.setPhone(resultSet.getString("phone"));
+				employee.setAddress(resultSet.getString("address"));
+				employee.setCreatedAt(resultSet.getTimestamp("created_at"));
+				employee.setUpdatedAt(resultSet.getTimestamp("updated_at"));
+				employee.setId(resultSet.getInt("id"));
+				employeeList.add(employee);
+			}				
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}		
+
+		return employeeList;
+	}
+
+	@Override
+	public List<Employee> selectByUsername(String username) {
+		String sql = "select * from employee where username=?";
+		List<Employee> employeeList = new ArrayList<>();
+		
+		try {
+			PreparedStatement preparedStatement = conn.prepareStatement(sql);
+			preparedStatement.setString(1, username);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			while(resultSet.next())
+			{
+				Employee employee = new Employee();
+				employee.setEmployeeNo(resultSet.getString("employee_no"));
+				employee.setUsername(resultSet.getString("username"));
+				employee.setPassword(resultSet.getString("password"));
+				employee.setName(resultSet.getString("name"));
+				employee.setPhone(resultSet.getString("phone"));
+				employee.setAddress(resultSet.getString("address"));
+				employee.setCreatedAt(resultSet.getTimestamp("created_at"));
+				employee.setUpdatedAt(resultSet.getTimestamp("updated_at"));
+				employee.setId(resultSet.getInt("id"));
+				employeeList.add(employee);
+			}				
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}		
+
+		return employeeList;
+	}
+
+	@Override
+	public void update(Employee employee) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
